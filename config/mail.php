a06911df642072d48222e7a4a324805199b2edbf
LTprograms <105554066+LTprograms@users.noreply.github.com>
2026-09-11 17:54:52 -0500

    .

--- config/mail.php
+++ config/mail.php
@@ -47,6 +47,8 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            // cPanel: el cert del SMTP a menudo es del hostname del servidor, no de mail.dominio.
+            'verify_peer' => filter_var(env('MAIL_VERIFY_PEER', true), FILTER_VALIDATE_BOOLEAN),
         ],
 
         'ses' => [
